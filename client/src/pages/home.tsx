3c63e51b108d85e07f75307b8e1aea928de43fe5
Nepail <kls18z@hotmail.com>
2023-02-15 17:39:04 +0800

    add react-apexcharts

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import { useList } from '@pankod/refine-core'
+
+import { Typography, Box, Stack } from '@pankod/refine-mui'
+
+import {
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  PropertyCard,
+  TopAgent
+} from 'components'
 
 const home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+        DashBoard
+
+        <Box mt="20px" display="flex"
+          flexWrap="wrap" gap={4}>
+          <PieChart
+            title="Properties for Sale"
+            value={684}
+            series={[75, 25]}
+            colors={['#475be8', '#e4e8ef']}
+          />
+          <PieChart
+            title="Properties for Rent"
+            value={550}
+            series={[60, 40]}
+            colors={['#475be8', '#e4b8ef']}
+          />
+          <PieChart
+            title="Total customers"
+            value={5684}
+            series={[75, 25]}
+            colors={['#275be8', '#c4e8ef']}
+          />
+          <PieChart
+            title="Properties for Cities"
+            value={555}
+            series={[75, 25]}
+            colors={['#475be8', '#e4e8ef']}
+          />
+        </Box>
+      </Typography>
+    </Box>
   )
 }
 
