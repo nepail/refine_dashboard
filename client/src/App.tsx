ef326e66910d1746eaa52fa971a4bd3c80f731b9
Nepail <kls18z@hotmail.com>
2023-02-16 14:14:04 +0800

    add CustomButton

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -126,7 +126,7 @@ function App() {
           catchAll={<ErrorComponent />}
           resources={[
             {
-              name: "property",
+              name: "properties",
               list: AllProperties,
               show: PropertyDetails,
               create: CreateProperty,
@@ -134,18 +134,18 @@ function App() {
               icon: <VillaOutlined />
             },
             {
-              name: "agent",
+              name: "agents",
               list: Agents,
               show: AgentProfile,
               icon: <PeopleAltOutlined />
             },
             {
-              name: "review",
+              name: "reviews",
               list: Home,
               icon: <StarOutlineRounded />
             },
             {
-              name: "message",
+              name: "messages",
               list: Home,
               icon: <ChatBubbleOutline />
             },
